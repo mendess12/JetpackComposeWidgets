ed9af6b0117a3ff21911d4a5f0be5866cfee79ae
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-03-28 13:15:40 +0300

    Add text, text field and button

--- app/src/main/java/com/yusufmendes/jetpackcomposewidgets/MainActivity.kt
+++ app/src/main/java/com/yusufmendes/jetpackcomposewidgets/MainActivity.kt
@@ -3,12 +3,22 @@ package com.yusufmendes.jetpackcomposewidgets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.yusufmendes.jetpackcomposewidgets.ui.theme.JetpackComposeWidgetsTheme
 
@@ -22,25 +32,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ScreenButtonTextAndTextField()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeWidgetsTheme {
-        Greeting("Android")
+        ScreenButtonTextAndTextField()
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScreenButtonTextAndTextField() {
+
+    val textFieldData = remember { mutableStateOf("") }
+    val getData = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Incoming Data : ${getData.value} ",
+            modifier = Modifier.background(Color.Green),
+            color = Color.Black
+        )
+        TextField(
+            value = textFieldData.value,
+            onValueChange = { textFieldData.value = it },
+            label = {
+                Text(text = "Enter Data")
+            })
+
+        Button(onClick = { getData.value = textFieldData.value }) {
+            Text(text = "Get Data")
+
+        }
+    }
+}
+
