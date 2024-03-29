4bc7adf5535c15b612a1e049bcf34a88398e4470
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-03-29 20:44:26 +0300

    Add text, text field and button privatisation

--- app/src/main/java/com/yusufmendes/jetpackcomposewidgets/MainActivity.kt
+++ app/src/main/java/com/yusufmendes/jetpackcomposewidgets/MainActivity.kt
@@ -3,11 +3,15 @@ package com.yusufmendes.jetpackcomposewidgets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -15,13 +19,20 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yusufmendes.jetpackcomposewidgets.ui.theme.JetpackComposeWidgetsTheme
 
 class MainActivity : ComponentActivity() {
@@ -65,16 +76,35 @@ fun ScreenButtonTextAndTextField() {
         Text(
             text = "Incoming Data : ${getData.value} ",
             modifier = Modifier.background(Color.Green),
-            color = Color.Black
+            color = Color.Black,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
         )
         TextField(
             value = textFieldData.value,
             onValueChange = { textFieldData.value = it },
             label = {
                 Text(text = "Enter Data")
-            })
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                colorResource(id = R.color.orange),
+                disabledTextColor = Color.Red,
+                focusedIndicatorColor = Color.Green,
+                focusedLabelColor = Color.Yellow
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
 
-        Button(onClick = { getData.value = textFieldData.value }) {
+        Button(
+            onClick = { getData.value = textFieldData.value },
+            colors = ButtonDefaults.buttonColors(
+                colorResource(id = R.color.white),
+                contentColor = Color.Blue
+            ),
+            border = BorderStroke(2.dp, Color.Blue),
+            shape = RoundedCornerShape(50)
+        ) {
             Text(text = "Get Data")
 
         }
